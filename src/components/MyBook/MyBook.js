7a70f041119b743d26f2063b148f9141c08f576e
eolculnamo2 <rbertram8@gmail.com>
2018-01-16 15:20:34 -0600

    map book data

--- src/components/MyBook/MyBook.js
+++ src/components/MyBook/MyBook.js
@@ -7,7 +7,11 @@ class MyBook extends React.Component{
         this.state = {
             authors: [],
             titles: [],
-            images: []
+            images: [],
+            categories: [],
+            recommendedBy: [],
+            tags: []
+
         }
     }
     componentDidMount(){
@@ -28,32 +32,96 @@ class MyBook extends React.Component{
             var authorHold = []
             var titlesHold = []
             var imagesHold = []
+            var recommendedByHold = []
+            var categoriesHold = []
+            var tagsHold = []
+
             data.forEach((x,i)=>{
                 authorHold.push(x.author)
                 titlesHold.push(x.title)
                 imagesHold.push(x.image)
+                if(x.recommendedBy.length === 0){
+                    recommendedByHold.push("n/a")
+                }
+                else if(x.recommendedBy.length>0){
+                recommendedByHold.push(x.recommendedBy)
+                }
+                categoriesHold.push(x.categories)
+                tagsHold.push(x.tags)
+                
             })
             this.setState({
                 authors: authorHold,
                 titles: titlesHold,
-                images: imagesHold
+                images: imagesHold,
+                recommendedBy: recommendedByHold,
+                categories: categoriesHold,
+                tags: tagsHold
             })
         })
         
     }
     render(){
         return(
-           <div>
+           <div className = "mybook-box">
                {this.state.titles.map((x,i)=>{
                    return(
-                       <div>
-                           <img src = {this.state.images[i]}/>
+                       <div className="mybook-child-box">
+                           <img className = "my-image" src = {this.state.images[i]}/>
+
+                           <div className = "my-book-info">
                            <h2>
                                {x}
                                </h2>
-                               <h4>
-                                   {this.state.authors[i]}
-                                   </h4>
+                               <h5 className ="author">
+                                   By: {this.state.authors[i]}
+                                   </h5>
+                                   <hr className = "hr-divider"/>
+                                   <h4 className = "organized-by">
+                                       Categories
+                                       </h4>
+                                           {this.state.categories[i].map((x)=>{
+                                               return <p className ="listed-organized-by">{x}</p>
+                                           })} 
+                                           <br/>
+
+                                    <h4 className = "organized-by">
+                                        Recommended By
+                                        </h4>
+                                        <span className ="listed-organized-by">
+                                            {this.state.recommendedBy[i]}
+                                            </span>
+                                            <br/>
+                                    <h4 className = "organized-by">
+                                        Tags
+                                        </h4>      
+                                            {this.state.tags[i].map((x)=>{
+                                                var tester;
+                                                alert(x)
+                                                if(x === ""){
+                                                    tester = "no tag";
+                                                }
+                                                else{
+                                                    tester = x
+                                                }
+                                                return <span className ="listed-organized-by">
+                                                {tester}
+                                                </span>
+                                            })}
+                                            
+                                   </div>
+
+                            <div className = "read-or-stars"> 
+                                <button className = "mark-button">
+                                    Mark as Read
+                                    </button><br/>
+                                    <p className = "book-link">
+                                        Audible
+                                        </p>
+                                        <p className = "book-link">
+                                        Amazon
+                                        </p>
+                                </div>
                            </div>
                    )
                })}
